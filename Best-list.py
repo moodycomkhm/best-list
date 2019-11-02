b3bb08eb0d92ea78a608ae7d7645684322ff4336
moodycomkhm <moodycomkhm99@gmail.com>
2019-11-02 03:40:45 +0200

    Create Best-list.py

---
+++ Best-list.py
@@ -0,0 +1,214 @@
+
+import os
+os.system('clear')
+os.system('printf "\x1b[35m" ') 
+name_list=(input('Entre name The LisT : ')+'.txt')
+os.system('clear') 
+os.system('printf "\033[3;36m"') 
+os.system('figlet best-list')
+
+R = "\x1b[31m"
+Y = "\x1b[33m"
+C = "\x1b[32m"
+M = "\x1b[35m"
+print(C+'By : '+R+'MoHaMeD AlMuSlEh') 
+print (Y+'My YouTube channel : https://www.youtube.com/channel/UCauk1RbXC-W25tFOskkldSw')
+print(" ")
+print ('Follow me on Instagram : www.instagram.com/mo_khm99')
+print(C+'----------------------------------------------------------------------------')
+print("\033[3;36m")
+n = ("\n")
+
+print (R+"Please don't mistake any information. That may make mistakes in the script ")
+print(" ")
+print (" ")
+print(" ")
+print("\x1b[35m")
+name=(input('first name '))
+str(name)
+father= (input ('father name ((optional)) '))
+str(father)
+last = (input ('last name '))
+str(last)
+born_date= (input ('born date yyyy '))
+int(born_date)
+num = (input ('Enter number phone ((optional)) '))
+int(num)
+
+
+list=open(name_list,'a')
+
+
+
+list.write(name.upper()+'123')
+list.write(n+name.lower()+'123')
+list.write(n+name.title()+'123')
+
+
+list.write(n+name.upper()+'12345')
+list.write(n+name.title()+'12345')
+list.write(n+name.lower()+'12345')
+
+
+list.write(n+name.upper()+'123456')
+list.write(n+name.title()+'123456')
+list.write(n+name.lower()+'123456')
+
+#()()()()()()()()()()))))))))))
+
+
+list.write(n+last.upper()+'123')
+list.write(n+last.lower()+'123')
+list.write(n+last.title()+'123')
+
+
+list.write(n+last.upper()+'12345')
+list.write(n+last.title()+'12345')
+list.write(n+last.lower()+'12345')
+
+
+list.write(n+last.upper()+'123456')
+list.write(n+last.title()+'123456')
+list.write(n+last.lower()+'123456')
+
+
+
+
+
+
+#()()()()()()()()))6)))))))))))
+
+
+list.write(n+name.upper()+' '+last.upper())
+list.write(n+name.lower()+' '+last.upper())
+list.write(n+name.title()+' '+last.upper())
+
+
+list.write(n+name.upper()+' '+last.lower())
+list.write(n+name.lower()+' '+last.lower())
+list.write(n+name.title()+' '+last.lower())
+
+
+list.write(n+name.upper()+' '+last.title())
+list.write(n+name.lower()+' '+last.title())
+list.write(n+name.title()+' '+last.title())
+
+
+#()()-_((_/(($($)$)$+$)))))))
+
+
+list.write(n+name.upper()+last.upper())
+list.write(n+name.upper()+last.lower())
+list.write(n+name.upper()+last.title())
+
+
+list.write(n+name.lower()+last.upper())
+list.write(n+name.lower()+last.lower())
+list.write(n+name.lower()+last.title())
+
+
+list.write(n+name.title()+last.title())
+list.write(n+name.title()+last.upper())
+list.write(n+name.title()+last.lower())
+
+
+#())()()()()()()!)!)!))))))
+j = ""
+k = " "
+if father != j and father != k :
+	
+
+    list.write(n+name.title()+father.title())
+    list.write(n+name.title()+father.upper())
+    list.write(n+name.title()+father.lower())
+
+
+
+
+    list.write(n+name.upper()+father.upper())
+    list.write(n+name.upper()+father.lower())
+    list.write(n+name.upper()+father.title())
+
+
+
+
+
+    list.write(n+name.lower()+father.upper())
+    list.write(n+name.lower()+father.lower())
+    list.write(n+name.lower()+father.title())
+
+
+
+#()()()()()()()()()()))))))))))
+
+
+list.write(n+name.upper()+born_date)
+list.write(n+name.lower()+born_date)
+list.write(n+name.title()+born_date)
+
+
+list.write(n+born_date+name.upper())
+list.write(n+born_date+name.lower())
+list.write(n+born_date+name.title())
+
+
+#()()()()()()()()())))))))))
+
+
+
+
+
+list.write(n+last.upper()+born_date)
+list.write(n+last.lower()+born_date)
+list.write(n+last.title()+born_date)
+
+
+list.write(n+born_date+last.upper())
+list.write(n+born_date+last.lower())
+list.write(n+born_date+last.title())
+
+
+#))(($($(($+$+$+$+$+$)))))))))
+
+if num != j and num != k :
+    list.write(n+name.upper()+num)
+    list.write(n+name.lower()+num)
+    list.write(n+name.title()+num)
+    list.write(n+num+name.upper())
+    list.write(n+num+name.lower())
+    list.write(n+num+name.title())
+    list.write(n+last.upper()+num)
+    list.write(n+last.lower()+num)
+    list.write(n+last.title()+num)
+    list.write(n+num+last.upper())
+    list.write(n+num+last.lower())
+    list.write(n+num+last.title())
+    list.write(n+num)
+print(" ")
+print (" ")
+print (R+"          File saved ")
+print(" ")
+
+print (Y+"ThAnKs for UsE our ScRiPt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
